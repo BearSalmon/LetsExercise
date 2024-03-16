f39cbd37759ae4097146a48c673d754b8f3825ae
salmoniscute <noralin249@gmail.com>
2024-03-16 18:10:17 +0800

    comparison basic structure

--- LetsExerciseUnity/Assets/Scripts/AnimationCode.cs
+++ LetsExerciseUnity/Assets/Scripts/AnimationCode.cs
@@ -13,39 +13,57 @@ public class AnimationCode : MonoBehaviour
     public UDPSend udpsend;
     public UDPReceive udpreceive;
 
-    public GameObject avatar;
+
+    public CountDownTimer countDownTimer;
+
+    public bool isAnimating ;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        lines = System.IO.File.ReadLines("Assets/PoseDataset/arm1.txt").ToList();
+        lines = System.IO.File.ReadLines("Assets/PoseDataset/arms/arm1.txt").ToList();
+        countDownTimer = GameObject.Find("ManagerToBeKeep").GetComponent<CountDownTimer>();
+        isAnimating = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        udpsend.SendData(counter.ToString());
-        string[] points = lines[counter ++].Split(',');
-
-        for (int i = 0; i <= 32; i++)
+        if (isAnimating)
         {
-            // normoalize position
-            //float x = float.Parse(points[0 + (i * 3)]) /20;
-            //float y = float.Parse(points[1 + (i * 3)]) /20;
-            //float z = float.Parse(points[2 + (i * 3)]) /20;
-            float x = float.Parse(points[0 + (i * 3)]) / 20;
-            float y = float.Parse(points[1 + (i * 3)]) / 20;
-            float z = float.Parse(points[2 + (i * 3)]) / 500;
+            udpsend.SendData(counter.ToString());
+            string[] points = lines[counter++].Split(',');
 
-            Body[i].transform.localPosition = new Vector3(x, y, z);
-        }
-        if (counter >= lines.Count)
-        {
-            counter = 0;
-            loop_cnt += 1;
-        }
+            for (int i = 0; i <= 32; i++)
+            {
+                // normoalize position
+                float x = float.Parse(points[0 + (i * 3)]) / 20;
+                float y = float.Parse(points[1 + (i * 3)]) / 20;
+                float z = float.Parse(points[2 + (i * 3)]) / 500;
 
-        while (!udpreceive.canContinue) ;
-        udpreceive.canContinue = false;
+                Body[i].transform.localPosition = new Vector3(x, y, z);
+            }
+            if (counter >= lines.Count)
+            {
+                counter = 0;
+                loop_cnt += 1;
+            }
+
+            while (!udpreceive.canContinue) ;
+            udpreceive.canContinue = false;
+
+        }
+        
+    }
+
+    public void StartAnimation()
+    {
+        isAnimating = true;
+    }
+
+    public void StopAnimation()
+    {
+        isAnimating = false;
     }
 }
--- LetsExerciseUnity/Assets/Scripts/CountDownTimer.cs
+++ LetsExerciseUnity/Assets/Scripts/CountDownTimer.cs
@@ -7,16 +7,21 @@ using UnityEngine.UI;
 
 public class CountDownTimer : MonoBehaviour
 {
-    float currentTime = -1f;
+    public float currentTime = -1f;
     [SerializeField] float startingTime = 30f;
     [SerializeField] TextMeshProUGUI count;
     public Image progressBar;
+
+
+    public AnimationCode animationCode;
 
     // Start is called before the first frame update
     void Start()
     {
         count.text = startingTime.ToString();
-        currentTime = startingTime;
+
+
+        animationCode = GameObject.Find("Manager").GetComponent<AnimationCode>();
 
         progressBar.fillAmount = 0f;
 
@@ -24,8 +29,11 @@ public class CountDownTimer : MonoBehaviour
 
     public void StartCountDown()
     {
+
         currentTime = startingTime;
         progressBar.fillAmount = 0f;
+
+        animationCode.StartAnimation();
     }
 
     void Update()
@@ -47,8 +55,9 @@ public class CountDownTimer : MonoBehaviour
         }
         else if (currentTime == 0)
         {
-          count.text = startingTime.ToString(); // Or any message you want when the countdown reaches zero
-          currentTime = -1;
+            count.text = startingTime.ToString(); // Or any message you want when the countdown reaches zero
+            currentTime = -1;
+            animationCode.StopAnimation();
         }
     }
 }
