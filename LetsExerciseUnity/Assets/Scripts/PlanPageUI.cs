0e528ec792d1a87022db16f481eecb1a477f8534
unknown <noralin249@gmail.com>
2024-05-22 22:41:26 +0800

    fix recommendation bug

--- LetsExerciseUnity/Assets/Scripts/PlanPageUI.cs
+++ LetsExerciseUnity/Assets/Scripts/PlanPageUI.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Runtime.InteropServices;
 
 public class PlanPageUI : MonoBehaviour
 {
@@ -10,8 +13,17 @@ public class PlanPageUI : MonoBehaviour
 
     public GameObject Pass;
     public GameObject UnPass;
+    public TextMeshProUGUI duration;
+
+    public RawImage arms;
+    public RawImage abs;
+    public RawImage legs;
+    public RawImage buttocks1;
+    public RawImage buttocks2;
 
     User user;
+
+    List<Pose> plan = new List<Pose>();
 
     void Start()
     {
@@ -20,6 +32,7 @@ public class PlanPageUI : MonoBehaviour
         buttonEvent = GameObject.Find("WholeManager").GetComponent<ButtonEvent>();
         user = dBUtils.GetUserByName(dBUtils.nowPlayer);
         setPlanPassOrNot();
+        setPlanSet();
     }
 
     void setPlanPassOrNot()
@@ -34,6 +47,79 @@ public class PlanPageUI : MonoBehaviour
             Pass.SetActive(true);
             UnPass.SetActive(false);
         }
+    }
+
+
+    int GetDuration(string level,int numOfPose )
+    {
+        if (level == "Easy")
+        {
+            return 55 * numOfPose;
+        }
+        else if (level == "Medium") return 70 * numOfPose;
+        else return 85 * numOfPose;
+    }
+
+    void setPlanSet()
+    {
+        plan.Clear();
+        string[] poseNames;
+        poseNames = user.RecommendationPoseSet.TrimEnd(',').Split(',');
+
+        int d = GetDuration(user.Level,poseNames.Length);
+        string min = (d / 60).ToString();
+        string sec = (d % 60).ToString();
+        duration.text = min + "m " + sec + "s";
+
+        foreach (string name in poseNames)
+        {
+            Pose pose = dBUtils.GetPoseByName(name);
+            plan.Add(pose);
+        }
+
+        Color color;
+        ColorUtility.TryParseHtmlString("#".ToString() + "CCC4C4", out color);
+
+        Color color2;
+        ColorUtility.TryParseHtmlString("#".ToString() + "FFC9AA", out color2);
+
+        Color color3;
+        ColorUtility.TryParseHtmlString("#".ToString() + "9494FF", out color3);
+
+        arms.color = color;
+        abs.color = color;
+        legs.color = color;
+        buttocks1.color = color;
+        buttocks2.color = color;
+        Debug.Log(user.PreferPart);
+
+        if (user.PreferPart == "Arms")
+        {
+            arms.color = color2;
+        }
+        else if (user.PreferPart == "Abs")
+        {
+            abs.color = color2;
+        }
+        else if (user.PreferPart == "Legs")
+        {
+            legs.color = color2;
+        }
+        else if (user.PreferPart == "Buttocks")
+        {
+            buttocks1.color = color2;
+            buttocks2.color = color3;
+        }
+        else
+        {
+            arms.color = color2;
+            abs.color = color2;
+            legs.color = color2;
+            buttocks1.color = color2;
+            buttocks2.color = color3;
+        }
+
+
     }
 
     // Update is called once per frame
