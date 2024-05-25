6ace74f8da880391ec505096bd96d2ca9c52df4f
unknown <noralin249@gmail.com>
2024-05-26 00:12:19 +0800

    fix calendar problem

--- LetsExerciseUnity/Assets/Scripts/CalendarUI.cs
+++ LetsExerciseUnity/Assets/Scripts/CalendarUI.cs
@@ -8,42 +8,7 @@ using UnityEngine.UI;
 
 public class CalendarUI : MonoBehaviour
 {
-    public class Day
-    {
-        public int dayNum;
-        public Color dayColor;
-        public Button obj;
-
-        public Day(int dayNum , Color dayColor , Button obj)
-        {
-            this.dayNum = dayNum;
-            this.dayColor = dayColor;
-            this.obj = obj;
-            obj.GetComponent<Image>().color = dayColor;
-
-        }
-
-        public void UpdateColor(Color newColor)
-        {
-            obj.GetComponent<Image>().color = newColor;
-            dayColor = newColor;
-        }
-
-        public void UpdateDay(int newDayNum)
-        {
-            this.dayNum = newDayNum;
-            if (dayColor == Color.white || dayColor == Color.green)
-            {
-                obj.GetComponentInChildren<TextMeshProUGUI>().text = (dayNum + 1).ToString();
-            }
-            else
-            {
-                obj.GetComponentInChildren<TextMeshProUGUI>().text = "";
-            }
-        }
-    }
-
-    private List<Day> days = new List<Day>();
+    
     public Button[] weeks;
     public TextMeshProUGUI Month;
     public DateTime currDate = DateTime.Now;
@@ -62,12 +27,19 @@ public class CalendarUI : MonoBehaviour
 
     public bool detailIsOpen;
 
+
+
     void Start()
     {
         dBUtils = GameObject.Find("WholeManager").GetComponent<DBUtils>();
-        UpdateCalendar(DateTime.Now.Year, DateTime.Now.Month);
         Detail.SetActive(false);
         detailIsOpen = false;
+
+    }
+
+    void Update()
+    {
+        UpdateCalendar(currDate.Year, currDate.Month);
     }
 
     int MonthStringToInt(string month)
@@ -145,55 +117,29 @@ public class CalendarUI : MonoBehaviour
         int startDay = GetMonthStartDay(year, month);
         int endDay = GetTotalNumberOfDays(year, month);
 
-
-        ///Create the days
-        ///This only happens for our first Update Calendar when we have no Day objects therefore we must create them
-
-        if (days.Count == 0)
+        for (int i = 0; i < 42; i++)
         {
-            for (int w = 0; w < 6; w++)
+            if (i < startDay || i - startDay >= endDay)
             {
-                for (int i = 0; i < 7; i++)
-                {
-                    Day newDay;
-                    int currDay = (w * 7) + i;
-                    if (currDay < startDay || currDay - startDay >= endDay)
-                    {
-                        newDay = new Day(currDay - startDay, Color.grey, weeks[w * 7 + i]);
-                    }
-                    else
-                    {
-                        newDay = new Day(currDay - startDay, Color.white, weeks[w * 7 + i]);
-                        
-                    }
-                    days.Add(newDay);
-                    days[w * 7 + i].UpdateDay(w * 7 + i - startDay);
-                }
+                weeks[i].GetComponent<Image>().color = Color.grey;
             }
-        }
-        ///loop through days
-        ///Since we already have the days objects, we can just update them rather than creating new ones
-        else
-        {
-            for (int i = 0; i < 42; i++)
+            else
             {
-                if (i < startDay || i - startDay >= endDay)
-                {
-                    days[i].UpdateColor(Color.grey);
-                }
-                else
-                {
-                    days[i].UpdateColor(Color.white);
-                }
-
-                days[i].UpdateDay(i - startDay);
+                weeks[i].GetComponent<Image>().color = Color.white;
+            }
+            if (weeks[i].GetComponent<Image>().color == Color.white)
+            {
+                weeks[i].GetComponentInChildren<TextMeshProUGUI>().text = (i - startDay + 1).ToString();
+            }
+            else
+            {
+                weeks[i].GetComponentInChildren<TextMeshProUGUI>().text = "";
             }
         }
 
-        ///This just checks if today is on our calendar. If so, we highlight it in green
         if (DateTime.Now.Year == year && DateTime.Now.Month == month)
         {
-            days[(DateTime.Now.Day - 1) + startDay].UpdateColor(Color.green);
+            weeks[(DateTime.Now.Day - 1) + startDay].GetComponent<Image>().color = Color.green;
         }
 
     }
@@ -222,7 +168,6 @@ public class CalendarUI : MonoBehaviour
             currDate = currDate.AddMonths(1);
         }
 
-        UpdateCalendar(currDate.Year, currDate.Month);
     }
 
 
